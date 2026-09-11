e6b1b4432da62036b6389c34a38311d007bb658b
Naomi994 <naomimante7@gmail.com>
2026-09-11 10:31:59 -0400

    Refactor RAM filling using indirect addressing
    Replace multiple direct RAM writes with a loop using indirect addressing.

--- Task_2.s
+++ Task_2.s
@@ -7,15 +7,17 @@
 fill_ram: 
   # Store FFh into RAM locations 50H - 58H using indirect ddressing
 
-  movb $0xFF, (%rbx) // This helps with indirect addressing 
-  movb $0xFF, ram+0x51 
-  movb $0xFF, ram+0x52
-  movb $0xFF, ram+0x53
-  movb $0xFF, ram+0x54
-  movb $0xFF, ram+0x55
-  movb $0xFF, ram+0x56
-  movb $0xFF, ram+0x57
-  movb $0xFF, ram+0x58
+  # This helps with indirect addressing
+  #  
+  movb $0xFF, (%eax) 
+  movb $0xFF, (%eax)
+  movb $0xFF, (%eax)
+  movb $0xFF, (%eax)
+  movb $0xFF, (%eax)
+  movb $0xFF, (%eax)
+  movb $0xFF, (%eax)
+  movb $0xFF, (%eax)
+  movb $0xFF, (%eax) 
 
 ret         # Return control back to C program 
 .section .note.GNU-stack,"",@progbits
