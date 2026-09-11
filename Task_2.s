7ff38f8451deb48f7043a239d1e9c0b8e4477e0f
Naomi994 <naomimante7@gmail.com>
2026-09-11 10:03:47 -0400

    Initialize RAM with 0xFF in specified locations (indirect addressing)
    Fill RAM locations 0x51 to 0x58 with 0xFF.

--- Task_2.s
+++ Task_2.s
@@ -1,1 +1,21 @@
+.section .bss
+.global ram 
+.lcomm ram, 256    # Reserve 236 butes of RAM (uninitialized memory)
 
+.section .text 
+.global fill_ram  # Make function visible to C program 
+fill_ram: 
+  # Store FFh into RAM locations 50H - 58H using direct addressing
+
+  movb $0xFF, (%rbx) // This helps with indirect addressing 
+  movb $0xFF, ram+0x51 
+  movb $0xFF, ram+0x52
+  movb $0xFF, ram+0x53
+  movb $0xFF, ram+0x54
+  movb $0xFF, ram+0x55
+  movb $0xFF, ram+0x56
+  movb $0xFF, ram+0x57
+  movb $0xFF, ram+0x58
+
+ret         # Return control back to C program 
+.section .note.GNU-stack,"",@progbits
