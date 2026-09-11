6dc334960e446f6398b035468e11d36d89b53c2a
Naomi994 <naomimante7@gmail.com>
2026-09-11 11:02:26 -0400

    Update print_ram.c

--- print_ram.c
+++ print_ram.c
@@ -1,6 +1,5 @@
 #include <stdio.h> 
-extern unsigned char ram[]; // RAM delcared in 
-assembly; 
+extern unsigned char ram[]; // RAM delcared in assembly 
 extern void fill_ram(void); // Assembly function 
 int main()
 {fill_ram(); // Run assembly vode 
